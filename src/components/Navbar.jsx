88db2352e36aa35050ffb830767debf4e4884823
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-11-26 08:59:35 +0530

    update src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ const Navbar = () => {
                     <div className="md:hidden">
                         <button
                             onClick={() => setIsOpen(!isOpen)}
-                            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
+                            className="bg-gray-700 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
                         >
                             {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
                         </button>
