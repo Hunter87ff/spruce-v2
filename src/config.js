c9684cc1d20007eb22c2cc9fa3fc258cbb29cc69
hunter87ff <huntergaming702@gmail.com>
2024-11-30 09:23:27 +0530

    Update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // configDotenv();
 
 export const API_ROUTE =  import.meta.env.VITE_API_ROUTE ||"http://localhost:3001/api";
-export const AUTH_URL = "https://discord.com/oauth2/authorize?client_id=931202912888164474&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&scope=guilds+identify"
+export const AUTH_URL = import.meta.env.VITE_AUTH_URL || "https://discord.com/oauth2/authorize?client_id=931202912888164474&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&scope=guilds+identify"
 export const DISCORD_API_BASE_URL = "https://discord.com/api/v10"
 export const DISCORD_AUTHORIZATION_BASE_URL = DISCORD_API_BASE_URL + "/oauth2/authorize"
 export const DISCORD_TOKEN_URL = DISCORD_API_BASE_URL + "/oauth2/token"
