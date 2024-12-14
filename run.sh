3c85ec6373af037f12a1f0adbd0883bfbada0f11
hunter87ff <huntergaming702@gmail.com>
2024-12-14 18:24:23 +0530

    Update run script to install global dependencies and build with Vite before starting the server

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-NODE_ENV=production && cd server && node app.js
+NODE_ENV=production
+npm install pm2 vite -g && npm install
+vite build
+# pm2 start server/app.js -i 2 --name "app"
+cd ./server && node app.js
