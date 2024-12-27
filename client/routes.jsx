5f3ec398a5f6374b77e7c2391648a7fe2ebca57c
hunter87ff <huntergaming702@gmail.com>
2024-12-27 20:44:14 +0530

    Remove Loading component and update routes accordingly

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,7 +2,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import E404 from "./pages/error/E404"
 import Dashboard from './pages/dash/Dashboard'
-import Loading from './pages/Loading'
 import {PrivacyPage, TermsPage} from "./pages/legal/pages"
 import AuthPage from './pages/AuthPage'
 
@@ -11,7 +10,6 @@ export const routes = [
     { path: '/about', component: About },
     { path: '*', component: E404 },
     { path: '/dashboard', component: Dashboard },
-    { path: '/loading', component: Loading },
     { path: '/privacy', component: PrivacyPage },
     { path: '/terms', component: TermsPage },
     { path: '/auth', component: AuthPage }
